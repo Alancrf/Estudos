2682f7942b6796bf041759694935070269adc618
Alan-PC <Alan-PC@DESKTOP-II4B69S>
2020-01-29 00:29:12 -0200

    update

--- src/model/Aritmetica.java
+++ src/model/Aritmetica.java
@@ -5,25 +5,46 @@ public class Aritmetica {
 	public int primeiroNumero;
 	public int segundoNumero;
 	
+	
 	public void soma (int primeiroNumero, int segundoNumero) {
 		
-		int result = 0, primeiroNumeroSeparado=0,segundoNumeroSeparado=0;
+		int result = 0;
 		
+		int[] primeiroSeparados;
+		int[] segundoSeparados;
+		int[] somaSeparados;
+		
+		primeiroSeparados = new int[3];
+		segundoSeparados = new int[3];
+		somaSeparados = new int [3];
+		int i = 0; 
 		while(primeiroNumero > 0) {
-		
-		primeiroNumeroSeparado = separaNumero(primeiroNumero);
-		System.out.println("Aqui é o primeiro número separado "+primeiroNumeroSeparado);
+			
+		primeiroSeparados[i] = separaNumero(primeiroNumero);
 		primeiroNumero = primeiroNumero/10;
+		i++;
 		
 		}
-		
+/*		
+		for(int cont = 0; cont < primeiroSeparados.length; cont++) {
+			System.out.println(primeiroSeparados[cont]);
+		}
+*/	
+		int j = 0;
 		while(segundoNumero > 0) {
 		
-		segundoNumeroSeparado = separaNumero(segundoNumero);
-		System.out.println("Aqui é o segundo número separado "+segundoNumeroSeparado);
+		segundoSeparados[j] = separaNumero(segundoNumero);
+		//System.out.println("Aqui é o segundo número separado "+segundoSeparados[j]);
 		segundoNumero = segundoNumero/10;
-			
+		j++;	
+		
 		}
+		
+		for(int contSoma = 0; contSoma < 3; contSoma++) {
+			somaSeparados[contSoma] = primeiroSeparados[contSoma] + segundoSeparados[contSoma];
+			System.out.println(somaSeparados[contSoma]);
+		}
+		
 		
 
 		//System.out.println("resultado é: "+primeiroNumeroSeparado+segundoNumeroSeparado);
